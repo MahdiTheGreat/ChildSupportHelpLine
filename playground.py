d62ca8048da7edc8b345bd735cdd5aa61b09d3f8
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2023-12-20 16:46:11 +0330

    changed the owl to rdf

--- playground.py
+++ playground.py
@@ -1,6 +1,6 @@
-from textblob import TextBlob
+# from textblob import TextBlob
 # from better_profanity import profanity
-# dirty_text = "fuck you?"
+# dirty_text = "I want to kill myself."
 # dirty_text_blob = TextBlob(dirty_text)
 # dirty_text_blob=dirty_text_blob.correct()
 # print(dirty_text_blob.sentiment)
@@ -9,17 +9,22 @@ from textblob import TextBlob
 
 from owlready2 import *
 owlready2.JAVA_EXE = "C:\\Program Files (x86)\\Java\\jre-1.8\\bin\\java.exe"
-onto = get_ontology("ChildSupportHelplineOntology.owl")
+onto = get_ontology("ChildSupportHelplineOntologyRdf.rdf")
 onto.load()
 responseTime=onto.ResponseTime("responseTime")
-responseTime.hasTime=[10]
+responseTime.hasTime=10
 temp=onto.get_instances_of(onto.ResponseTime)
 with onto:
  sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
- onto.save("buffer.owl")
-buffer_onto = get_ontology("buffer.owl")
-temp1=buffer_onto.get_instances_of(buffer_onto.ResponseTime)
+
+
+ #onto.save("buffer.owl")
+# buffer_onto = get_ontology("buffer.owl")
+# buffer_onto.load()
+
+temp1=onto.get_instances_of(onto.ResponseTime)
 for instance in temp1:
+ print(instance.hasTime)
  print(instance.isShortResponseTime)
 
 

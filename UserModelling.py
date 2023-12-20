a52450ccd76f7b5aca6df0492d62293cdfaa0287
Tanguy Cauquil <cauquil.tanguy@gmail.com>
2023-12-21 00:32:24 +0100

    Correct typeSpeed Type

--- UserModelling.py
+++ UserModelling.py
@@ -1,15 +1,16 @@
 from owlready2 import *
 from transformers import pipeline
 
-messageNb = 0
-
 def sentimentAnalysis(message):
+    """ Compute polarity from the message """ 
+
     sentiment_pipeline = pipeline("sentiment-analysis")
     data = [message]
     result = sentiment_pipeline(data)[0]['label']
     return result
 
 
+messageNb = 0
 def updateUserModel(message, ontology):
     """ Updates message instance by adding informations on sentiment analysis """
     
@@ -31,11 +32,11 @@ def updateUserModel(message, ontology):
     messageNb +=1
 
 def updateTrollProbability(conversation, ontology):
-    """ Computes the new probability for the user to be a troll """
+    """ Computes the new probability of the"""
     pass
 
 def updateTypingSpeed(conversation, ontology):
-    """ Updates the typing speed of the user model """
+    """ Update the typing speed of the user model"""
     user = conversation[-1].sender
     supportSeekerNbMessages = len(list(filter(lambda message: message.sender == user, conversation)))
     dt = conversation[-1].time - conversation[-2].time
@@ -43,9 +44,9 @@ def updateTypingSpeed(conversation, ontology):
 
     typing_speed = messageSize/dt
 
-    userInd = ontology.User[user.value]
-    currentAvg = userInd.TypingSpeed
+    currentAvg = ontology.TypingSpeed[0].hasSpeed
 
     updatedAvg = (currentAvg * (supportSeekerNbMessages-1) + typing_speed) / supportSeekerNbMessages
-    userInd.TypingSpeed = updatedAvg
+    ontology.TypingSpeed[0].hasSpeed = updatedAvg
+
 
